65c2d11659633ef46f2806b67f693bdc5e4dcdb9
Slawomir Osiak <slawek-osiak@o2.pl>
2018-04-01 12:58:36 +0200

    main-right-site

--- js/script.js
+++ js/script.js
@@ -3,4 +3,17 @@ $(document).ready(function() {
 		$(".languages-toggle:not(#languages)").hide(800);
 		$("#languages").toggle(800);
 	});
+	$(".public-item").click(function(){
+		$(".public-toggle:not(#public)").hide(400);
+		$("#public").toggle(400);
+	});
+	$(".search-icon").click(function() {
+		$(".icon").animate({
+			left: "95%"
+		}, 1000, function() {
+			//Complete//
+		});
+		$(".code-button").slideDown(400).delay(600);
+		$(".code-input").slideDown(600).delay(800);
+	});
 });
