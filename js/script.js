115632e04479e8d55d0d931e29ca2e4737b121c2
Slawomir Osiak <slawek-osiak@o2.pl>
2018-04-02 23:27:13 +0200

    edit search

--- js/script.js
+++ js/script.js
@@ -9,13 +9,17 @@ $(document).ready(function() {
 	});
 	$(".search-passive").click(function() {
 		$(".search-dark").css("display", "block");
-		$(".search-dark").css("width", "100%");
+		$(".nav-left").css("padding-top", "45px");
+		$(".search-dark").css("left", "-33%");
+		$(".search-dark").css("width", "133%");
 		$(".search-passive").css("display", "none");
 		$(".search-active-icon").css("display", "block");
 	});
 	$(".search-active-icon").click(function() {
+		$(".search-dark").css("left", "0");
 		$(".search-dark").css("width", "1%").fadeOut(300);
 		$(".search-active-icon").css("display", "none");
 		$(".search-passive").css("display", "block");
+		$(".nav-left").css("padding-top", "5px");
 	});
 });
